d793856df945272a9fd6b28ead7696b758a020dd
Maksim Kulishevich <maxim8992012@gmail.com>
2026-07-07 19:42:15 +0300

    feat: install new dependences and delete unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { TopPageModule } from './top-page/top-page.module';
-import { ProductModule } from './product/product.module';
-import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    imports: [MongooseModule.forRoot("mongodb://localhost/test")],
-    AuthModule,
-    TopPageModule,
-    ProductModule,
-    ReviewModule,
+    MongooseModule.forRoot('mongodb://localhost/test'),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
